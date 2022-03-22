7bea17f5be1ca63e21b401f3c813001e84ffed1e
“xuexiong” <xuexiong@souche.com>
2022-03-22 14:07:19 +0800

    feat:增加链表反转的循环写法

--- algorithm-set/src/main/java/com/xue/demo/algorithm/set/Link.java
+++ algorithm-set/src/main/java/com/xue/demo/algorithm/set/Link.java
@@ -12,9 +12,46 @@ public class Link {
         NodeBean nodeBean = new NodeBean();
         nodeBean.ramdomInit();
         nodeBean.printSelf();
-
+        System.out.print("链表反转递归:");
         NodeBean bean = reverseList(nodeBean);
         bean.printSelf();
+
+
+        NodeBean nodeBean2 = new NodeBean();
+        nodeBean2.ramdomInit();
+        nodeBean2.printSelf();
+        System.out.print("链表反转循环:");
+        NodeBean bean2 = reverseForList(nodeBean2);
+        bean2.printSelf();
+
+    }
+
+    /**
+     * 链表反转 循环
+     *
+     * @param nodeBean2
+     * @return
+     */
+    private static NodeBean reverseForList(NodeBean root) {
+        if (root == null || root.getNext() == null) {
+            return root;
+        }
+        //前一个节点
+        NodeBean preNode = null;
+        //当前节点
+        NodeBean nowNode = root;
+
+        while (nowNode != null) {
+            //下一个节点 临时存储,因为当前节点反转后，会丢失下一个节点，所以需要提前存储
+            NodeBean nextNode = nowNode.getNext();
+            //当前节点反转
+            nowNode.setNext(preNode);
+
+            //节点后移
+            preNode = nowNode;
+            nowNode = nextNode;
+        }
+        return preNode;
 
     }
 
@@ -26,15 +63,15 @@ public class Link {
      * @author xuexiong@souche.com  15:31 2020-04-16
      **/
     public static NodeBean reverseList(NodeBean root) {
-        if(root==null||root.getNext()==null){
+        if (root == null || root.getNext() == null) {
             return root;
         }
         //得到当前节点的下一节点
-        NodeBean nextNode=root.getNext();
+        NodeBean nextNode = root.getNext();
         //打断当前指针链
         root.setNext(null);
         //每次递归下一节点进行反转
-        NodeBean re=reverseList(nextNode);
+        NodeBean re = reverseList(nextNode);
         //反转指针域
         nextNode.setNext(root);
         return re;
